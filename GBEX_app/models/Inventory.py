b2af1f68596fd93ad60d24c3d15a8f9e58c72604
cfblaeb <laeb@dtu.dk>
2020-07-16 14:33:46 +0200

    updated models and added culturemedia and lots of batches

--- GBEX_app/models/Inventory.py
+++ GBEX_app/models/Inventory.py
@@ -11,7 +11,6 @@ from .models import BaseOption, GBEXModelBase, AbstractBatch, default_order, def
 
 class InventoryItem(GBEXModelBase):
 	Usage = models.TextField(blank=True, null=True)
-	Location = models.TextField(blank=True, null=True)
 	menu_label = "Inventory"
 
 	class Meta:
@@ -28,7 +27,7 @@ class Plasmid(InventoryItem):
 	Antibiotic = models.ManyToManyField(AntibioticOption, blank=True)
 	Genbank_file = ResumableFileField(blank=True, null=True, upload_to=get_upload_path, max_length=500)
 
-	order = [*default_order, 'CommonName', 'Usage', 'Antibiotic', 'Genbank_file', 'Location', 'Batches']
+	order = [*default_order, 'CommonName', 'Usage', 'Antibiotic', 'Genbank_file', 'Batches']
 	symbol = "PL"
 	col_display_func_dict = {
 		'Batches': lambda item: f"<a href='{reverse('list_PlasmidBatch', kwargs=dict(parent_pk=item.pk))}'>{item.plasmidbatch_set.filter(archived=False).count()} batches</a>",
@@ -48,6 +47,7 @@ class Plasmid(InventoryItem):
 class PlasmidBatch(AbstractBatch):
 	Parent = models.ForeignKey(Plasmid, on_delete=models.PROTECT)
 	Barcode = models.TextField(blank=True, null=True)
+	Location = models.TextField(blank=True, null=True)
 	SequenceVerified = models.BooleanField(default=False)
 
 	order = [*default_order, 'Barcode', 'SequenceVerified', 'Parent']
@@ -58,6 +58,7 @@ class PlasmidBatch(AbstractBatch):
 
 class Primers(InventoryItem):
 	Sequence = models.TextField(blank=True, null=True)
+	Location = models.TextField(blank=True, null=True)
 
 	order = [*default_order, "Usage", "Sequence", "Location"]
 	symbol = "PR"
@@ -79,18 +80,17 @@ class Strain(InventoryItem):
 	Genotype = models.TextField(blank=True, null=True)
 	Vendor = models.ForeignKey(VendorOption, blank=True, null=True, on_delete=models.PROTECT)
 	CatalogNo = models.TextField(blank=True, null=True)
-	TubesLeft = models.PositiveIntegerField(blank=True, null=True)
-	TubeVolume = models.PositiveIntegerField(blank=True, null=True)
 	Plasmids = models.ManyToManyField(Plasmid, blank=True)
 	ParentStrain = models.ForeignKey("self", null=True, blank=True, on_delete=models.PROTECT)
 	Genbank_file = ResumableFileField(blank=True, null=True, upload_to=get_upload_path, max_length=500)
 
 	order = [*default_order, 'CommonName', 'Species', 'Subtype', 'Usage', 'ParentStrain', 'Antibiotic', 'Genotype',
-			 'Plasmids', 'Genbank_file', 'Vendor', 'CatalogNo', 'TubesLeft', 'TubeVolume', 'Location']
+			 'Plasmids', 'Genbank_file', 'Vendor', 'CatalogNo', 'Batches']
 	symbol = "ST"
 	col_display_func_dict = {
 		'Plasmids': lambda item: ", ".join(ab.name for ab in item.Plasmids.all()) if item.Plasmids.all() else "",
 		'Antibiotic': lambda item: ", ".join(ab.name for ab in item.Antibiotic.all()) if item.Antibiotic.all() else "",
+		'Batches': lambda item: f"<a href='{reverse('list_StrainBatch', kwargs=dict(parent_pk=item.pk))}'>{item.strainbatch_set.filter(archived=False).count()} batches</a>",
 	}
 	widgets = {
 		**default_widgets,
@@ -101,16 +101,80 @@ class Strain(InventoryItem):
 		'Antibiotic': autocomplete.ModelSelect2Multiple(url=reverse_lazy('AntibioticOption-autocomplete')),
 	}
 
+	col_html_string = ['Genbank_file', 'Batches']
+	col_read_only = [*default_readonly, 'Batches']
+
+
+class StrainBatch(AbstractBatch):
+	Parent = models.ForeignKey(Strain, on_delete=models.PROTECT)
+	Location = models.TextField(blank=True, null=True)
+	Barcode = models.TextField(blank=True, null=True)
+	TubesLeft = models.PositiveIntegerField(blank=True, null=True)
+
+	order = [*default_order, 'TubesLeft', 'Barcode', 'Location', 'SequenceVerified', 'Parent']
+	symbol = "ST_Batch"
+
+	col_read_only = [*default_readonly, 'Parent']
+
 
 class CellLine(InventoryItem):
 	CommonName = models.TextField(blank=True, null=True)
 	Species = models.ForeignKey(SpeciesOption, on_delete=models.PROTECT)
 	Genotype = models.TextField(blank=True, null=True)
 
-	order = [*default_order, 'CommonName', 'Usage', 'Species', 'Genotype', 'Location']
+	order = [*default_order, 'CommonName', 'Usage', 'Species', 'Genotype', 'Batches']
 	symbol = "CL"
+
+	col_display_func_dict = {
+		'Batches': lambda item: f"<a href='{reverse('list_CellLineBatch', kwargs=dict(parent_pk=item.pk))}'>{item.celllinebatch_set.filter(archived=False).count()} batches</a>",
+	}
 
 	widgets = {
 		**default_widgets,
 		'Species': autocomplete.ModelSelect2(url=reverse_lazy('SpeciesOption-autocomplete')),
 	}
+
+	col_html_string = ['Batches']
+	col_read_only = [*default_readonly, 'Batches']
+
+
+class CellLineBatch(AbstractBatch):
+	Parent = models.ForeignKey(CellLine, on_delete=models.PROTECT)
+	Location = models.TextField(blank=True, null=True)
+	Barcode = models.TextField(blank=True, null=True)
+	TubesLeft = models.PositiveIntegerField(blank=True, null=True)
+	Mycoplasma = models.DateField(blank=True, null=True)
+
+	order = [*default_order, 'Location', 'Barcode', 'TubesLeft', 'Mycoplasma', 'Parent']
+	symbol = "CL_Batch"
+
+	col_read_only = [*default_readonly, 'Parent']
+
+
+class CultureMedia(InventoryItem):
+	ProductName = models.TextField(blank=True, null=True)
+	Vendor = models.ForeignKey(VendorOption, blank=True, null=True, on_delete=models.PROTECT)
+	CatalogNo = models.TextField(blank=True, null=True)
+
+	order = [*default_order, 'ProductName', 'Vendor', 'CatalogNo', 'Batches']
+	symbol = "CM"
+
+	col_display_func_dict = {
+		'Batches': lambda item: f"<a href='{reverse('list_CultureMediaBatch', kwargs=dict(parent_pk=item.pk))}'>{item.culturemediabatch_set.filter(archived=False).count()} batches</a>",
+	}
+
+	col_html_string = ['Batches']
+	col_read_only = [*default_readonly, 'Batches']
+
+
+class CultureMediaBatch(AbstractBatch):
+	Parent = models.ForeignKey(CellLine, on_delete=models.PROTECT)
+	Location = models.TextField(blank=True, null=True)
+	Barcode = models.TextField(blank=True, null=True)
+	TubesLeft = models.PositiveIntegerField(blank=True, null=True)
+	Mycoplasma = models.DateField(blank=True, null=True)
+
+	order = [*default_order, 'Location', 'Barcode', 'TubesLeft', 'Mycoplasma', 'Parent']
+	symbol = "CM_Batch"
+
+	col_read_only = [*default_readonly, 'Parent']
