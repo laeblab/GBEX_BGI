01fca70f45a34bfb31fb7f0bf8cc60d2e5cbff4e
cfblaeb <laeb@dtu.dk>
2020-11-08 00:07:54 +0100

    updated the Transaction model for better quartzy import

--- GBEX_app/models/Purchasing.py
+++ GBEX_app/models/Purchasing.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 from .models import GBEXModelBase, default_order
 
 menu_label = "Purchasing"
@@ -7,6 +8,9 @@ menu_label = "Purchasing"
 class Transaction(GBEXModelBase):
 	what = models.TextField()
 	price = models.PositiveIntegerField()
+	budget = models.PositiveIntegerField()
+	order_date = models.DateField(default=datetime.date.today)
+	quartzy_import = models.BooleanField(default=False)
 	menu_label = menu_label
 	symbol = "TRA"
-	order = [*default_order, 'what', 'price']
+	order = [*default_order, 'what', 'price', 'budget', 'quartzy_import']
