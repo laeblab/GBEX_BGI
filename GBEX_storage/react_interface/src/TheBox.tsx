aa4db3b754fa1adb438bd54ac0b48eb4c0c77707
cfblaeb <laeb@dtu.dk>
2021-07-13 00:25:37 +0200

    resizing TheBox for optimal sizing

--- GBEX_storage/react_interface/src/TheBox.tsx
+++ GBEX_storage/react_interface/src/TheBox.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState, createRef, useEffect } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		box_root: {
-			height: "100%"
 		},
 		wells: {
 			textAlign: 'center',
@@ -18,19 +17,34 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export default function TheBox(props: {columns: number, rows: number}) {
-	const { columns, rows } = props
+	const {columns, rows} = props
 	const classes = useStyles();
+	const divRef: React.RefObject<HTMLDivElement> = createRef()
+	const [dimensions, setDimensions] = useState({width: 1, height: 2})
+	useEffect(() => {
+		if (divRef.current) {
+			const {current} = divRef
+			const boundingRect = current.getBoundingClientRect()
+			const {width, height} = boundingRect
+			setDimensions({width: Math.round(width), height: Math.round(height)})
+		}
+	}, [divRef])
+
+	const square_size = {
+		height: dimensions.width / columns,
+		width: dimensions.width / columns
+	}
 
 	return (
-		<Box className={classes.box_root} display="flex" flexGrow={1} flexDirection="column">
-			{[...Array(columns)].map((e, i) => {
+		<div ref={divRef} className={classes.box_root}>
+			{[...Array(rows)].map((e, i) => {
 				return (
 					<Box display="flex" flexGrow={1} key={i}>
-						{[...Array(rows)].map((ee, ii) => {
-							return <Box className={classes.wells} flexGrow={1} key={ii}>{i}, {ii}</Box>})
+						{[...Array(columns)].map((ee, ii) => {
+							return <Box className={classes.wells} style={square_size} key={ii}>{i}, {ii} {dimensions.width}</Box>})
 						}
 					</Box>)
 			})}
-		</Box>
+		</div>
 	);
 }
