14ca65d4119f2fac86b5692f81398ef97d5664bf
cfblaeb <laeb@dtu.dk>
2022-01-30 00:23:54 +0100

    huhu

--- GBEX_app/admin.py
+++ GBEX_app/admin.py
@@ -1,25 +1,9 @@
 from django.contrib import admin
-from django.contrib.contenttypes.fields import GenericRelation
 from reversion_compare.admin import CompareVersionAdmin
 from rest_framework.authtoken.admin import TokenAdmin
 from django.apps import apps
 from glob import glob
 from importlib import import_module
-
-# needed for integration with GBEX_storage to link Vial
-from GBEX_storage.models import Vial
-from django.contrib.contenttypes.admin import GenericTabularInline
-from django import forms
-
-
-class VialForm(forms.ModelForm):
-	class Meta:
-		model = Vial
-		fields = '__all__'
-
-
-class InlineVial(GenericTabularInline):
-	model = Vial
 
 
 TokenAdmin.raw_id_fields = ['user']
@@ -29,10 +13,5 @@ model_modules = [import_module(x.replace("/", ".")[:-3]) for x in model_files]
 
 
 for model in apps.get_app_config('GBEX_app').get_models():
-	# check for generic relationships to the Vial model from GBEX_storage
-	add_vial_inline = {}
-	if any([gen for gen in model._meta.get_fields() if isinstance(gen, GenericRelation) and gen.related_model is Vial]):
-		add_vial_inline = {"inlines": [InlineVial], "form": VialForm}
-
-	modeladmin = type(f"{model.__name__}Admin", (CompareVersionAdmin,), {**add_vial_inline})
+	modeladmin = type(f"{model.__name__}Admin", (CompareVersionAdmin,), {})
 	admin.site.register(model, modeladmin)
--- GBEX_storage/urls.py
+++ GBEX_storage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import get_locs_and_boxes
+from .views import get_locs_and_boxes, get_vial_and_maybe_parent
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='GBEX_storage/index.html'), name='StorageIndex'),
     path('locsNboxs', get_locs_and_boxes, name='loc_box'),
+    path('displayVial/<int:id>', get_vial_and_maybe_parent, name='vial_disp'),
 ]
